d901bf9b0e806fae4a36522bbc8f067c615ddc03
Vilhelm <vilhelm.engstrom@tuta.io>
2022-09-25 10:42:46 +0200

    Add type hints

--- buildbot_extensions/steps/docker.py
+++ buildbot_extensions/steps/docker.py
@@ -3,13 +3,22 @@
 from buildbot.steps import shell, shellsequence  # pylint: disable=import-error
 from buildbot.plugins import util  # pylint: disable=import-error
 
+from typing import List, Optional
+
 
 class Docker(shell.ShellCommand):
     """Wrapper for running shell commands inside docker container"""
 
     name = "docker"
 
-    def __init__(self, image, command, workdir=None, volumes=None, **kwargs):
+    def __init__(
+        self,
+        image: str,
+        command: List[str],
+        workdir: Optional[str] = None,
+        volumes: Optional[List[str]] = None,
+        **kwargs,
+    ):
         self._image = image
         self.command = ["docker", "run", "--rm", "-t", "--net=host"]
 
@@ -24,18 +33,13 @@ class Docker(shell.ShellCommand):
         self.command += command
         super().__init__(**kwargs)
 
-    @property
-    def image(self):
-        """Docker image name"""
-        return self._image
-
 
 class Build(shell.ShellCommand):
     """Shell wrapper building docker images"""
 
     name = "docker build"
 
-    def __init__(self, tag, path=".", **kwargs):
+    def __init__(self, tag: str, path: str = ".", **kwargs):
         self.command = [
             "docker",
             "build",
@@ -61,7 +65,13 @@ class Volume(shellsequence.ShellSequence):
 
     name = "docker volume"
 
-    def __init__(self, volume, permissions=None, image=None, **kwargs):
+    def __init__(
+        self,
+        volume: str,
+        permissions: str = None,
+        image: Optional[str] = None,
+        **kwargs,
+    ):
         permissions = permissions if permissions else "755"
         image = image if image is not None else "archlinux:latest"
         self._volume = volume
@@ -80,8 +90,3 @@ class Volume(shellsequence.ShellSequence):
             util.ShellArg(command=dpfx + [f"chmod -R {permissions} /{volume}"]),
         ]
         super().__init__(commands=commands, **kwargs)
-
-    @property
-    def volume(self):
-        """Get name of the volume"""
-        return self._volume
